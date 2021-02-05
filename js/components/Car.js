94f86b6627fb25185d55fd86d50c5ef25222f32b
Julijava <76951723+Julijava@users.noreply.github.com>
2021-02-05 17:11:25 +0200

    Initial commit

--- js/components/Car.js
+++ js/components/Car.js
@@ -3,58 +3,53 @@ class Car {
         this.ok = true;
         this.engine = false;
         this.mileage = 0;
-        this.tank = tank;                   // litres
-        this.consumption = consumption;     // litres/100km
+        this.tank = tank;                       // litres
+        this.consumption = consumption;         // litres/100km
     }
 
     turnOn() {
         if (!this.ok) {
-            console.log('Masina neveikia');
+            console.log('Masina neveikia...');
             return false;
         }
         if (this.engine) {
             this.ok = false;
             this.engine = false;
-            console.log('Sveikinu, sugadinai starteri!');
+            console.log('Sveikinu, sugadinai starteri! 🥳🥳🥳');
             return false;
         }
         this.engine = true;
-        console.log('Variklis ijungtas');
+        console.log('Varyklis ijungtas!');
     }
-    
 
     turnOff() {
         if (!this.engine) {
-            console.log('Isjungto variklio dar karta isjungti nemoku');
+            console.log('Isjungto varyklio dar karta isjungti nemoku...');
             return false;
         }
         this.engine = false;
-        console.log('Variklis isjungtas');
+        console.log('Varyklis isjungtas!');
     }
-        
 
     drive(distance) {
         if (!this.engine) {
-            console.log('Norint vaziuoti - ijunk varikli');
+            console.log('Norint vaziuoti - ijunk varykli!');
             return false;
         }
 
-         const maxDistanceAvailable = this.tank / this.consumption * 100;
-         if (maxDistanceAvailable >= distance) {
-             this.mileage += distance;
-             this.tank -= distance * this.consumption / 100;
-             console.log('Nuvaziavo: ', distance);
-         } else {
-             this.mileage += maxDistanceAvailable;
-             console.log('Nuvaziavo: ', maxDistanceAvailable);
-             this.tank = 0;
-             this.turnOff();
-             console.log(`bake per mazai norimam atstumui: nuvaziavo
-              ${maxDistanceAvailable} ir liko nenuvaziuoti ${distance - 
-                maxDistanceAvailable}`);
-         }
+        const maxDistanceAvailable = this.tank / this.consumption * 100;
+        if (maxDistanceAvailable >= distance) {
+            this.mileage += distance;
+            this.tank -= distance * this.consumption / 100;
+            console.log('Nuvaziavo: ', distance);
+        } else {
+            this.mileage += maxDistanceAvailable;
+            console.log('Nuvaziavo: ', maxDistanceAvailable);
+            this.tank = 0;
+            this.turnOff();
+            console.log(`Bake per mazai norimam atstumui: nuvaziavo ${maxDistanceAvailable} ir liko nenuvaziuoti ${distance - maxDistanceAvailable}`);
+        }
     }
-
 }
 
-export { Car }
+export { Car } 
--- js/main.js
+++ js/main.js
@@ -4,12 +4,10 @@ const renoBegom = new Car(5, 2.5);
 
 console.log(renoBegom);
 renoBegom.turnOn();
-renoBegom.turnOn();
-renoBegom.turnOn();
+
 renoBegom.drive(20);
 renoBegom.drive(20);
 renoBegom.drive(20);
 renoBegom.drive(200);
 
-
-console.log(renoBegom);
+console.log(renoBegom); 
